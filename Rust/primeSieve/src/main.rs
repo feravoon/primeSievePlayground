e676de64006b793ccd53d7f6cd9939ce71e3d557
Ahmet Çakıroğlu <ahmetcakiroglu@Ahmets-Mac-mini.local>
2023-05-17 21:59:11 +0300

    Improvements

--- Rust/primeSieve/src/main.rs
+++ Rust/primeSieve/src/main.rs
@@ -1,60 +1,64 @@
 use std::time::{Duration, Instant};
 
 fn main() {
-    const SIEVE_SIZE: usize =  1000000;
-
+    const SIEVE_SIZE: usize = 1000000;
     let start: Instant = Instant::now();
     let mut end: Instant;
     let mut dur: Duration;
     let mut passes: usize = 0;
     let mut is_prime: [u8; SIEVE_SIZE+1] = [0; SIEVE_SIZE+1];
-    //let mut is_prime: Box<[u8; SIEVE_SIZE+1]> = Box::new([0; SIEVE_SIZE+1]);
     let time_limit: f64 = 5.0;
+
     loop
     {
-        is_prime.fill(0);
-        for i in (1..SIEVE_SIZE).step_by(2) {
-            is_prime[i] = 1;
-        }
+        let mut x: usize = 1;
+        let alternate = || {x = x+1; (x%2) as u8};   
+        is_prime.fill_with(alternate);
         
-        let mut k: usize;
-        let mut curr_num: usize;
-
-        is_prime[0] = 0;
         is_prime[1] = 0;
         is_prime[2] = 1;
 
         let upper_limit: usize = (SIEVE_SIZE as f32).sqrt() as usize;
+        let mut curr_num: usize;
         for i in 3..upper_limit
         {
-            if is_prime[i]==1
+            if is_prime[i] == 1
             {
-                k = i;
-                loop
-                {
-                    curr_num = i*k;
-                    if curr_num > SIEVE_SIZE
-                    {
-                        break;
-                    }
-                    
+                curr_num = i*i;
+                while curr_num <= SIEVE_SIZE
+                {  
                     is_prime[curr_num] = 0;
-                    k = k+2;
+                    curr_num += 2*i;
                 }
             }
         }
         end = Instant::now();
-        passes = passes+1;
+        passes += 1;
         dur = end-start;
 
-        if dur.as_secs_f64() > time_limit
+        if dur.as_secs_f64() >= time_limit
         {
             break;
-        }
-            
+        }     
     }
     println!("----------------------------------------------------");
     println!("It ran {} passes in {} seconds.", passes, dur.as_secs_f64());
     println!("Performance: {} passes/sec", (passes as f64)/dur.as_secs_f64());
+
+    let mut prime_count: usize = 0;
+    for i in 0..SIEVE_SIZE
+    {
+            prime_count += is_prime[i] as usize;
+    }
+
+    if prime_count == 78498
+    {
+        println!("And, it's working correctly!");
+    }
+    else
+    {
+        println!("But, it's not working correctly!");
+    }
+    
     println!("----------------------------------------------------");
 }
